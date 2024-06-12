1d9f8cec228b22a24518ef961bf3a4fbf9488a5e
Huy-2k3 <nguyenhuy04072019@gmail.com>
2024-06-12 12:30:00 +0700

    sửa lại đường dẫn ở productprovider

--- tuan4/lib/data/provider/productprovider.dart
+++ tuan4/lib/data/provider/productprovider.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 class ReadData {
   Future<List<Product>> loadData () async {
     var data = await rootBundle.loadString(
-      "assets/files/categorylist.json");
+      "assets/files/productlist.json");
       var dataJson = jsonDecode(data);
       return (dataJson['data'] as List).map((e) => Product.fromJson(e)).toList();
   }
